5e80ffa8b5213b9938bc82d2daea9ab875f70d8d
Shreyasi Prasad <57710288+Shreyasi3521@users.noreply.github.com>
2024-06-14 16:31:53 +0530

    Update StatusResource.py codeQL added extra line

--- mage_ai/api/resources/StatusResource.py
+++ mage_ai/api/resources/StatusResource.py
@@ -1,5 +1,6 @@
 import os
 import socket
+
 from mage_ai.api.resources.GenericResource import GenericResource
 from mage_ai.cluster_manager.constants import ClusterType
 from mage_ai.data_preparation.models.constants import MAX_PRINT_OUTPUT_LINES
